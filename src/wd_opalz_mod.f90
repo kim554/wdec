61e921f9bcd12aa996ea4fb1d3584d615afa62e8
kim554 <agnesbischoff@gmail.com>
2023-07-05 17:02:13 -0400

    Updated file for v20

--- src/wd_opalz_mod.f90
+++ src/wd_opalz_mod.f90
@@ -1,22 +1,72 @@
 module kappa
-
   use kap_lib
   use kap_def
   use chem_lib
   use chem_def
   use const_def
   use const_lib
-  use crlibm_lib
+  use math_lib
+  use utils_lib, only: mesa_error
 
   implicit none
+  !this program demonstrates how to use mesa/kap in a stellar structure code
+  !it reads in a mesa/star model of an AGB star so that it makes full use
+  !of mesa/kap's capabilities. modules kap_lib and kap_def contain access to
+  !all of the pieces required to set up and use mesa/kap.
 
-  character(len=256) :: kappa_file_prefix, kappa_CO_prefix, kappa_lowT_prefix
-  logical :: use_cache, cubic_X_interpolation, &
-       cubic_Z_interpolation, include_electron_conduction, &
-       use_Zbase_for_Type1
-  integer :: handle2, ikfirst
-  !  integer :: handle, i, ii
+  !  character(len=256) :: model_file, output_file
+  integer :: handle2, ifirst
+  integer, parameter :: NSpec = 7
+  logical :: use_cache
+  type (Kap_General_Info), pointer :: rq2
   integer :: ierr
+
+  integer, parameter :: h1 = 1
+  integer, parameter :: h2 = 2
+  integer, parameter :: he3 = 3
+  integer, parameter :: he4 = 4
+  integer, parameter :: li7 = 5
+  integer, parameter :: be7 = 6
+  integer, parameter :: b8 = 7
+  integer, parameter :: c12 = 8
+  integer, parameter :: c13 = 9
+  integer, parameter :: n13 = 10
+  integer, parameter :: n14 = 11
+  integer, parameter :: n15 = 12
+  integer, parameter :: o16 = 13
+  integer, parameter :: o17 = 14
+  integer, parameter :: o18 = 15
+  integer, parameter :: f19 = 16
+  integer, parameter :: ne20 = 17
+  integer, parameter :: ne21 = 18
+  integer, parameter :: ne22 = 19
+  integer, parameter :: na22 = 20
+  integer, parameter :: na23 = 21
+  integer, parameter :: mg24 = 22
+  integer, parameter :: mg25 = 23
+  integer, parameter :: mg26 = 24
+  integer, parameter :: al26 = 25
+  integer, parameter :: al27 = 26
+  integer, parameter :: si28 = 27
+  integer, parameter :: si29 = 28
+  integer, parameter :: si30 = 29
+  integer, parameter :: p31 = 30
+  integer, parameter :: s32 = 31
+
+!  real(dp) :: Mstar, Xc, Xn, Xo, Xne, xc_base, xn_base, xo_base, xne_base, &
+!       lnfree_e, d_lnfree_e_dlnRho, d_lnfree_e_dlnT, &
+!       eta, d_eta_dlnRho, d_eta_dlnT
+  real(dp) :: Z_init
+!  real(dp) :: lnRho(maxpts), lnT(maxpts), logRho(maxpts), logT(maxpts), X(maxspec,maxpts)
+!  real(dp) :: X(maxspec,maxpts)
+!  real(dp) :: lnR(maxpts), L, dq(maxpts)
+!  real(dp) :: kappa(maxpts), dlnkap_dlnRho(maxpts), dlnkap_dlnT(maxpts)
+!  real(dp) :: kappa_fracs(num_kap_fracs), dlnkap_dxa(maxspec)
+!  real(dp) :: kappaCO(maxpts), dlnkapCO_dlnRho(maxpts), dlnkapCO_dlnT(maxpts)
+!  real(dp) :: kappaCO_fracs(num_kap_fracs), dlnkapCO_dxa(maxspec)
+  character (len=32) :: my_mesa_dir
+
+  integer, dimension(:), pointer :: chem_id, net_iso
 
 contains
 
@@ -41,118 +91,83 @@ contains
   !  opr = d_ln_opacity/d_ln_rho 
   !  opt = d_ln_opacity/d_ln_T 
   !*****************************************************************************************************
-  subroutine opalz(t,ro, xa, fkappa, opr, opt)
-
-    real(dp) :: Xc, Xn, Xo, Xne, xc_base, xn_base, xo_base, xne_base, &
-         zbar, frac_Type2, lnfree_e, d_lnfree_e_dlnRho, d_lnfree_e_dlnT
+  subroutine opalz(t, ro, xa, fkappa, opr, opt)
     real(dp), intent(in) :: t, ro
+    real(dp), dimension(NSpec), intent(in) :: xa
     real(dp), intent(out) :: fkappa, opr, opt
-    real(dp) :: logRho, logT
-    real(dp) :: Z_init, Z, type2_logT_lower_bdy, Zbase
-    real(dp) :: kappa_blend_logT_upper_bdy, kappa_blend_logT_lower_bdy
-    real(dp) :: kap_Type2_full_off_X = 0.71d0, kap_Type2_full_on_X = 0.70d0
-    real(dp) :: kap_Type2_full_off_dZ = 0.001d0, kap_Type2_full_on_dZ = 0.01d0
-    real(dp), dimension(7), intent(in) :: xa
-    real(dp), dimension(7) :: amass, nion
-    real(dp), parameter, dimension(7) :: Aion = [1., 4., 12., 14., 16., 20.18, 24.31]
-    real(dp), parameter, dimension(7) :: Zion = [1., 2., 6., 7., 8., 10., 12.]
-    character (len=32) :: my_mesa_dir
-
-    kappa_file_prefix = 'gn93'
-    !kappa_lowT_prefix = 'lowT_Freedman11'
-    kappa_lowT_prefix = 'lowT_fa05_gn93'
-
-    kappa_CO_prefix = '' ! use default
-    kappa_lowT_prefix = '' ! use default
-    kappa_blend_logT_upper_bdy = 0 ! use default
-    kappa_blend_logT_lower_bdy = 0 ! use default
-    type2_logT_lower_bdy = 0 ! use default
-    use_cache = .false.
-    ierr = 0
-    use_Zbase_for_Type1 = .false.
+    real(dp) :: logt, logro
+    real(dp) :: lnfree_e, d_lnfree_e_dlnRho, d_lnfree_e_dlnT, eta, d_eta_dlnRho, d_eta_dlnT
+    real(dp) :: kappa_fracs(num_kap_fracs), dlnkap_dxa(NSpec)
+    integer :: i
 
     ! initialization and setup
-
-    if (ikfirst .ne. 1) then
-
+    if (ifirst .ne. 1) then
        write(*,*) 'loading MESA opacity tables...'
-       ! my_mesa_dir = '../..'         
-       my_mesa_dir = ''         
+       
+       ierr = 0
+       use_cache = .false.
+       Z_init = 0.0
+
+       my_mesa_dir = ''
        call const_init(my_mesa_dir,ierr)     
        if (ierr /= 0) then
           write(*,*) 'const_init failed'
-          stop 1
+          call mesa_error(__FILE__,__LINE__)
        end if
 
-       call crlibm_init
+       call math_init()
 
        call chem_init('isotopes.data', ierr)
-       call kap_init( &
-            kappa_file_prefix, kappa_CO_prefix, kappa_lowT_prefix, &
-            kappa_blend_logT_upper_bdy, kappa_blend_logT_lower_bdy, &
-            type2_logT_lower_bdy, use_cache, '', ierr) 
-       if(ierr/=0) stop 'problem in kap_init'
+       call kap_init(use_cache, '', ierr) 
+       if(ierr/=0) call mesa_error(__FILE__,__LINE__,'problem in kap_init')
 
        !next it is necessary to create a 'handle' for the general kap structure
        !using handles, it is possible to simultaneously access more than one
        !working copy of the opacity subroutines with different settings
-       handle2 = alloc_kap_handle(ierr)
-       if(ierr/=0) stop 'problem in alloc_kap_handle'
+!       handle1 = alloc_kap_handle_using_inlist('inlist_sample', ierr)
+!       call kap_ptr(handle1, rq1, ierr)
+!       rq1% use_Type2_opacities = .false.
+!       rq1% Zbase = Z_init
 
+       handle2 = alloc_kap_handle_using_inlist('inlist_sample', ierr)
+       call kap_ptr(handle2, rq2, ierr)
+       rq2% use_Type2_opacities = .true.
+       rq2% Zbase = Z_init
 
-       !the final step is to set the type of X and Z interpolation, either linear or cubic spline
-       !this is achieved by two logical variables, in this example both X and Z use cubic splines
-       cubic_X_interpolation = .true.
-       cubic_Z_interpolation = .true.
-       include_electron_conduction = .true.
-       call kap_set_choices( &
-            handle2, cubic_X_interpolation, cubic_X_interpolation, &
-            include_electron_conduction, &
-            kap_Type2_full_off_X, kap_Type2_full_on_X, &
-            kap_Type2_full_off_dZ, kap_Type2_full_on_dZ, &
-            ierr)
-       if(ierr/=0) stop 'problem in kap_set_interpolation_choices'
-       !end of initialization and setup
+       if(ierr/=0) call mesa_error(__FILE__,__LINE__,'problem in alloc_kap_handle')
 
-       ikfirst=1
-    end if
+       allocate(chem_id(NSpec), net_iso(num_chem_isos))
+       !chem_id(:) = (/ ih1, ih2, ihe3, ihe4, ili7, ibe7, ib8, ic12, &
+       !     ic13, in13, in14, in15, io16, io17, io18, if19, ine20, ine21, &
+       !     ine22, ina22, ina23, img24, img25, img26, ial26, ial27, &
+       !     isi28, isi29, isi30, ip31, is32 /)
+       chem_id(:) = (/ ih1, ihe4, ic12, in14, io16, ine20, img24 /)
+       net_iso(:) = 0
+       do i = 1, NSpec
+          net_iso(chem_id(i)) = i
+       end do
 
-!    zbar = 1.5d0              ! needed for electron conduction at high rho
+       ifirst = 1
+
+    endif
+
+    ! these should come from an eos call
     lnfree_e = 0d0            ! needed for Compton at high T
     d_lnfree_e_dlnRho = 0d0
     d_lnfree_e_dlnT = 0d0
+    eta = 0d0            ! needed for Compton at high T
+    d_eta_dlnRho = 0d0
+    d_eta_dlnT = 0d0
 
-    XC  = xa(3)
-    XN  = xa(4)
-    XO  = xa(5)
-    XNe = xa(6)
-    Z = 1d0 - (xa(1)+xa(2))
+    logt  = log10(t)
+    logro = log10(ro)
 
-    nion(:) = xa(:)/Aion(:)
-    nion(:) = nion(:)/sum(nion(:))
-    zbar = sum(nion(:)*Zion(:))
-    ! write(*,'(10f9.4)') xa(:)
-    ! write(*,'(10f9.4)') nion(:)
-    ! write(*,'(10f9.4)') Zion(:)
-    ! write(*,'(10f9.4)') Aion(:)
-    ! write(*,*) 'zbar = ',zbar
-
-    logT=log10(t)
-    logRho=log10(ro)
-    Zbase = 0.d0
-
-    !  subroutine opalz(t,ro, xa, fkappa, opr, opt)
-    call kap_get_Type2( &
-         handle2, zbar, xa(1), Z, Zbase, XC, XN, XO, XNe, logRho, logT, &
-         lnfree_e, d_lnfree_e_dlnRho, d_lnfree_e_dlnT, use_Zbase_for_Type1, &
-         frac_Type2, fkappa, opr, opt, ierr)
-    if(ierr/=0) write(*,*) 'kap_get_Type2 failed'
-
-
-
-    !all finished? then deallocate the handle and unload the opacity tables
-!    call free_kap_handle(handle2)
-!    call kap_shutdown
+    call kap_get( &
+         handle2, Nspec, chem_id, net_iso, xa, logro, logt, lnfree_e, d_lnfree_e_dlnRho, &
+         d_lnfree_e_dlnT, eta, d_eta_dlnRho, d_eta_dlnT, kappa_fracs, fkappa, &
+         opr, opt, dlnkap_dxa, ierr)
+    if(ierr/=0) write(*,*) 'kap_get (Type 2) failed at i=', i
+    ! write(*,*) 'kappa_fracs:',kappa_fracs
 
   end subroutine opalz
 
